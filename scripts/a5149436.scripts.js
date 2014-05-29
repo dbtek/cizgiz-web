70ac6ff35fe1a7a28e0e50711897cb46c19885e2
İsmail Demirbilek <ce.demirbilek@gmail.com>
2014-05-30 01:24:01 +0300

    dist release

---
+++ scripts/a5149436.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("cizgizWebApp",["ngRoute"]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("cizgizWebApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
