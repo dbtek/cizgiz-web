bd970fb5aa4cfe909e02912f6eff867ac4922cfa
İsmail Demirbilek <ce.demirbilek@gmail.com>
2015-07-09 15:25:03 +0300

    api endpoint added, grunt task added

--- Gruntfile.js
+++ Gruntfile.js
@@ -284,6 +284,11 @@ module.exports = function (grunt) {
           cwd: '.tmp/images',
           dest: '<%= yeoman.dist %>/images',
           src: ['generated/*']
+        }, {
+          expand: true,
+          cwd: 'api',
+          dest: '<%= yeoman.dist %>/api',
+          src: ['*']
         }]
       },
       styles: {
